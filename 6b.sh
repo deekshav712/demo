412da7bd01ca522d462d690a661a2fcbf40fff47
deekshav712 <98947371+deekshav712@users.noreply.github.com>
2022-02-24 13:48:58 +0530

    Update 6b.sh

--- 6b.sh
+++ 6b.sh
@@ -1,3 +1,6 @@
+#Write s a shell script to find a file/s that matches a pattern given as command line argument
+#in the home directory, display the contents of the file and copy the file into the directory
+#~/mydir.
 for i in `ls`
 do
 fl=`expr length $i`
