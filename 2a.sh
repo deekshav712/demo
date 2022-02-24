ed837c69e7bdd3fb938f4109edb86c1580f373d4
deekshav712 <98947371+deekshav712@users.noreply.github.com>
2022-02-24 13:45:19 +0530

    Update 2a.sh

--- 2a.sh
+++ 2a.sh
@@ -1,3 +1,6 @@
+#Write a shell script that accepts two filenames as arguments, checks if the permissions for
+#these files are identical and if the permissions are identical, output common permissions
+#otherwise output each filename followed by its permissions.
 echo "enter the first file:"
 read file1
 if [ -e $file1 ]
