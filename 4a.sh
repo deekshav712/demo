3a7b14f2b7a07267d26fee73f832c049c9b9e7d7
deekshav712 <98947371+deekshav712@users.noreply.github.com>
2022-02-24 13:47:15 +0530

    Update 4a.sh

--- 4a.sh
+++ 4a.sh
@@ -1,3 +1,5 @@
+#Write a shell script that accept one or more file names as argument and convert all of them
+#to uppercase, provided they exists in current directory.
 for i in $*
 do
 echo "----$i contents are-----"
