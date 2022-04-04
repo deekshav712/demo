315343e6e8ed65bfa4d58321a5d96f25ea90048b
deekshav712 <98947371+deekshav712@users.noreply.github.com>
2022-04-04 16:15:27 +0530

    current date replaced by * or **

--- shell program/5b.sh
+++ shell program/5b.sh
@@ -1,3 +1,5 @@
+#Write a shell script to display the calendar for the current month with current date replaced
+#by * or ** depending whether the date is one digit or two digit.
 d=`date +%d`
 d=`expr $d + 0`
 l=`cal | tr -d "[\137][\010]" | grep -wn "$d" | head -c 1`
