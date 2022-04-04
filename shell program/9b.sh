d19dce5f54bef350febc27ae9942926154154d1f
deekshav712 <98947371+deekshav712@users.noreply.github.com>
2022-04-04 15:50:47 +0530

    folds long lines into 40 columns.

--- shell program/9b.sh
+++ shell program/9b.sh
@@ -1,3 +1,7 @@
+#Write a shell script that folds long lines into 40 columns. Thus any line that exceeds 40
+#characters must be broken after 40th, a “/” is to be appended as the indication of folding and
+#processing is to be continued with the residue. The input is to be supplied through a text file
+created by the user.
 echo -n"Enter a filename:"
 read filename
 if [ -f $filename ]
