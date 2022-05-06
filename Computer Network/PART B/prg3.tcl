62a91f6ab4b23fe5011efecda7926ec167f3e5ba
deekshav712 <98947371+deekshav712@users.noreply.github.com>
2022-05-06 15:23:26 +0530

    Ethernet Lan

---
+++ Computer Network/PART B/prg3.tcl
@@ -0,0 +1,104 @@
+#===================================
+#     Simulation parameters setup
+#===================================
+set val(stop)   10.0                         ;# time of simulation end
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Open the NS trace file
+set tracefile [open out.tr w]
+$ns trace-all $tracefile
+
+#Open the NAM trace file
+set namfile [open out.nam w]
+$ns namtrace-all $namfile
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 6 nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+$n0 label "src1"
+$n2 label "src2"
+$n3 label "dest2"
+$n5 label "dest1"
+$n0 color "magenta"
+$n2 color "magenta"
+
+#===================================
+#        Links Definition        
+#===================================
+#Createlinks between nodes
+$ns duplex-link $n0 $n2 100.0Mb 10ms DropTail
+$ns queue-limit $n0 $n2 50
+$ns duplex-link $n2 $n1 100.0Mb 10ms DropTail
+$ns queue-limit $n2 $n1 50
+$ns duplex-link $n2 $n3 100.0Mb 10ms DropTail
+$ns queue-limit $n2 $n3 50
+
+set lan [$ns newLan "$n3 $n4 $n5" 1Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel]
+
+#Give node position (for NAM)
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n2 $n1 orient left-down
+$ns duplex-link-op $n2 $n3 orient right
+
+#===================================
+#        Agents Definition        
+#===================================
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n0 $tcp0
+
+set sink1 [new Agent/TCPSink]
+$ns attach-agent $n5 $sink1
+$ns connect $tcp0 $sink1 
+
+set tcp1 [new Agent/TCP]
+$ns attach-agent $n2 $tcp1
+
+set sink2 [new Agent/TCPSink]
+$ns attach-agent $n3 $sink2
+$ns connect $tcp1 $sink2
+
+#===================================
+#        Applications Definition        
+#===================================
+#Setup a FTP Application over TCP connection
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
+$ns at 1.0 "$ftp0 start"
+$ns at 2.0 "$ftp0 stop"
+
+#Setup a FTP Application over TCP connection
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+$ns at 1.0 "$ftp1 start"
+$ns at 2.0 "$ftp1 stop"
+
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam out.nam &
+    exit 0
+}
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "finish"
+$ns at $val(stop) "puts \"done\" ; $ns halt"
+$ns run
