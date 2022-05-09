4a98c56cf15d6bd7c9fe034cb9c2995d9a9a0c6a
deekshav712 <98947371+deekshav712@users.noreply.github.com>
2022-05-09 16:15:04 +0530

    maximum length of any file

--- shell program/1a.sh
+++ shell program/1a.sh
@@ -1,3 +1,6 @@
+#Write a shell script that takes a valid directory name as a argument recursively descend all
+#the sub-directors, find the maximum length of any file in that hierarchy and writ the maximum
+#value to the standard output.
 if [ $# -eq 1 ]
 then
 if [ -d $1 ]
