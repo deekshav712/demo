de23f516d92a85abf9b0320571487e5bb440aa8b
Deeksha V <98947371+deekshav712@users.noreply.github.com>
2022-05-13 16:37:20 +0530

    starting and ending line number

--- shell program/9a.sh
+++ shell program/9a.sh
@@ -1,3 +1,5 @@
+#Write a shell script that accepts the filename, starting and ending line number as an
+#argument and display all the lines between the given line number.
 if [ $# -eq 3 ]
 then
 	if [ -f $1 ]
