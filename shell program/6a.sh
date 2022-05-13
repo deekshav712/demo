b75e188ed4786f2bf9bfada4d56fae655560aaea
Deeksha V <98947371+deekshav712@users.noreply.github.com>
2022-05-13 16:35:50 +0530

    file/s that matches a pattern

--- shell program/6a.sh
+++ shell program/6a.sh
@@ -1,3 +1,7 @@
+#Write s a shell script to find a file/s that matches a pattern given as command line argument
+#in the home directory, display the contents of the file and copy the file into the directory
+#~/mydir.
+
 if [ $# -eq 1 ]
 then
 if [ -d ~/mydir ]
