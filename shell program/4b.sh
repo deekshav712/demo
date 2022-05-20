39292d90e6a40e1dd28621b38f17edffbdb3155d
Deeksha V <98947371+deekshav712@users.noreply.github.com>
2022-05-20 16:23:08 +0530

    display all links to a file

--- shell program/4b.sh
+++ shell program/4b.sh
@@ -1,3 +1,8 @@
+#Write a shell script that displays all the links to a file specified as the first argument to the
+#script. The second argument, which is optional, can be used to specify in which the search is to
+#begin. If this second argument is not present, the search is to begin in the current working
+#directory. In either case, the starting directory as well as its subdirectories at all levels must be
+#searched. The script need not include error checking
 if [ $# -ge 1 ]
 then
    if [ $# -eq 2 ]
