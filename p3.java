35169de9bd496897f0c38920010987e80d7e9a5b
Deeksha V <98947371+deekshav712@users.noreply.github.com>
2022-07-04 15:46:22 +0530

    adding 2 num

---
+++ p3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class p3 {public static void main(String[] args) {
+    int a,b,c;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter value for a and b");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=a+b;
+    System.out.println(c);
+}
+    
+}
