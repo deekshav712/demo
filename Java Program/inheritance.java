f3815cd197c8f46ee494264de3329016812af2a5
Deeksha V <98947371+deekshav712@users.noreply.github.com>
2022-08-01 15:25:13 +0530

    inheritance(program 4)

---
+++ Java Program/inheritance.java
@@ -0,0 +1,27 @@
+package mca018;
+
+interface Rectangle {
+    float area_rect(float x,float y);
+}
+ interface Triangle {
+    float area_tri(float x,float y);
+
+ }
+
+ class Area implements Triangle,Rectangle {
+    public float area_rect(float x,float y) {
+        return(x*y);
+    }
+    public float area_tri(float x,float y) {
+        return(x*y)/2;
+    }
+
+ }
+public class inheritance {
+    public static void main(String[] args) {
+        Area a=new Area();
+        System.out.println("Area of rectangle: "+a.area_rect(20,30));
+        System.out.println("Area of triangle: "+a.area_tri(20,30));
+    }
+    
+}
