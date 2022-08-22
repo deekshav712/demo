86605d1514230f9adc0071c867747044737bf3f3
Deeksha V <98947371+deekshav712@users.noreply.github.com>
2022-08-22 16:46:55 +0530

    square

---
+++ Java Program/Program 7/square.java
@@ -0,0 +1,14 @@
+package shape;
+
+public class square {
+    float side;
+    public void getData(float temp)
+    {
+        side=temp;
+    }
+    public double area()
+    {
+        return(side*side);
+    }
+    
+}
