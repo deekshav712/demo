4ae2d63dc3e140cf2b7208904b0edde3f422c61a
Deeksha V <98947371+deekshav712@users.noreply.github.com>
2023-03-09 06:53:08 +0530

    8.py

--- IOT/8.py
+++ IOT/8.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as gpio
 import time
 import datetime
+
 led=13
+switch1=35
 
 gpio.setmode(gpio.BOARD)
 gpio.setwarnings(False)
 gpio.setup(led,gpio.OUT,initial=0)
-gpio.setup(led,gpio.OUT)
+gpio.setup(switch1,gpio.IN)
 
 from flask import Flask,render_template
 app=Flask(__name__)
@@ -24,6 +26,16 @@ def glow_led(event):
 	elif event==switch1 and light_status=='ON':
 		gpio.output(led1,True)
 		light_status="OFF"
+		
+@app.route('/')
+def led_status():
+	now=datetime.datetime.now()
+	timeString=now.strftime("%H:%M:%D-%m-%y")
+	templateData= {
+	'status':Light_status,
+	'time':timeString
+	}
+	return render_template('light_status.html',**templateData)
 		
 #for LED
 @app.route('/redledon')
@@ -49,7 +61,7 @@ def redledoff():
 	return render_template('light_status.html',**templateData)
 
 gpio.add_event_detect(switch1,gpio.RISING,callback = glow_led,bouncetime = 1)
-gpio.add_event_detect(switch2,gpio.RISING,callback = glow_led,bouncetime = 1)
+#app.run(debug=True,port=4000,host='192.168.14.16')
 try:
 	while(True):
 		time.sleep(1)
