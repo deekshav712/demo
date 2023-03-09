cf42838fa358f09603aeb394eedb1cada4a38923
Deeksha V <98947371+deekshav712@users.noreply.github.com>
2023-03-09 19:47:42 +0530

    LED status

--- IOT/Program 8/Program 8.py
+++ IOT/Program 8/Program 8.py
@@ -1,20 +1,13 @@
 import RPi.GPIO as gpio
 import time
 import datetime
+
 led=13
-buzz=36
-relay = 38
-#timeString=now.strftime("%Y-%m-%d %H:%M")
+
 gpio.setmode(gpio.BOARD)
 gpio.setwarnings(False)
 gpio.setup(led,gpio.OUT,initial=0)
 gpio.setup(led,gpio.OUT)
-
-gpio.setup(buzz,gpio.OUT,initial=0)
-gpio.setup(buzz,gpio.OUT)
-
-gpio.setup(relay,gpio.OUT,initial=0)
-gpio.setup(relay,gpio.OUT)
 
 from flask import Flask,render_template
 app=Flask(__name__)
@@ -22,6 +15,7 @@ app=Flask(__name__)
 @app.route('/')
 def hello_world():
 	return render_template('web.html')
+
 #for LED
 @app.route('/redledon')
 def redledon():
@@ -45,53 +39,5 @@ def redledoff():
 	}
 	return render_template('web.html',**templateData)
 	
-#for buzzer
-@app.route('/buzzon')
-def buzzon():
-	gpio.output(buzz,gpio.LOW)
-	now=datetime.datetime.now()
-	timeString=now.strftime("%Y-%m-%d %H:%M")
-	templateData={
-	'bstatus':'ON',
-	'time':timeString
-	}
-	return render_template('web.html',**templateData)
-	
-@app.route('/buzzoff')
-def buzzoff():
-	gpio.output(buzz,gpio.HIGH)
-	now=datetime.datetime.now()
-	timeString=now.strftime("%Y-%m-%d %H:%M")
-	templateData={
-	'bstatus':'OFF',
-	'time':timeString
-	}
-	
-	return render_template('web.html',**templateData)
-
-#for realy
-@app.route('/relayon')
-def realyon():
-	gpio.output(buzz,gpio.LOW)
-	now=datetime.datetime.now()
-	timeString=now.strftime("%Y-%m-%d %H:%M")
-	templateData={
-	'rstatus':'ON',
-	'time':timeString
-	}
-	return render_template('web.html',**templateData)
-	
-@app.route('/realyoff')
-def realyoff():
-	gpio.output(buzz,gpio.HIGH)
-	now=datetime.datetime.now()
-	timeString=now.strftime("%Y-%m-%d %H:%M")
-	templateData={
-	'rstatus':'OFF',
-	'time':timeString
-	}
-	
-	return render_template('web.html',**templateData)
-
 if __name__=="__main__":
 	app.run(debug=True,port=4000,host='172.16.201.210:4000')
